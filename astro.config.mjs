aa49a918b35acc4f597dd0b6152e53bd89158633
ddk070 <fxuan070@gmail.com>
2026-08-31 17:11:32 +0800

    改回了自己的網址

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://justanote-web.vercel.app/',
 	integrations: [mdx(), sitemap(), pagefind()],
 	fonts: [
 		{
